580cc549484a6ee8a8ab510760d33c420dfd20b1
Noor Muhammad <20013146@mydbs.ie>
2024-12-06 21:09:05 +0000

    call return fecth function

--- mitigation.py
+++ mitigation.py
@@ -3,3 +3,4 @@ def login_secure(username, password):
     query = "SELECT * FROM users WHERE username = ? AND password = ?"
     print(f"Executing secure query: {query} with parameters ({username}, {password})")
     cursor.execute(query, (username, password))
+    return cursor.fetchall()
