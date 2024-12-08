c71ce61a4c56890445cb69d03e6964efc1057390
Noor Muhammad <20013146@mydbs.ie>
2024-12-08 16:48:19 +0000

    Added private attck key through new key generation

--- key_exchange/exchangekey.py
+++ key_exchange/exchangekey.py
@@ -10,3 +10,10 @@ class MaliciousKeyExchanger:
     
 #Create RSA key Pair for malicious purpose
     def create_attack_keypair(self):
+        attacker_private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        attacker_public_key = attacker_private_key.public_key()
+        return attacker_private_key, attacker_public_key
+    
