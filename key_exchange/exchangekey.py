b5e91f366f9a60f656e6ca3cc0b467ef9e054685
Noor Muhammad <20013146@mydbs.ie>
2024-12-08 16:45:56 +0000

    add class Malicious Key exchnager

--- key_exchange/exchangekey.py
+++ key_exchange/exchangekey.py
@@ -2,3 +2,10 @@ import requests
 import base64
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+
+#Class MaliciousKey Exchanger
+class MaliciousKeyExchanger:
+    def __init__(self):
+        self.compromised_keys = {}
+    
+#Create RSA key Pair for malicious purpose
